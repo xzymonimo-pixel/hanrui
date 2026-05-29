9f942e993dc068c585cbccce94dedf44469ff3ec
xzymonimo-pixel <xzymonimo@keio.jp>
2026-05-30 06:52:44 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-=======
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
->>>>>>> fresh-main
   images: {
     unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> fresh-main
